23ef30c3bdecdc0d29606d672e42fb468fedd049
ndunguwaweru-dan <109993033+ndunguwaweru-dan@users.noreply.github.com>
2026-06-09 14:43:06 +0300

    Add files via upload

---
+++ AiotApplication.java
@@ -0,0 +1,13 @@
+package com.iot.ai.aiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiotApplication.class, args);
+	}
+
+}
